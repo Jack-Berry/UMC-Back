b4512e8b8e91b2379786842b64585f6374ffca7a
Your Name <jackdanielberry@gmail.com>
2025-09-19 18:32:10 +0100

    cors stuff

--- app.js
+++ app.js
@@ -41,14 +41,14 @@ const allowedOrigins = [
 const corsOptions = {
   origin: (origin, callback) => {
     if (!origin || allowedOrigins.includes(origin)) {
-      callback(null, origin);
+      callback(null, true);
     } else {
       callback(new Error("Not allowed by CORS"));
     }
   },
   methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
   allowedHeaders: ["Content-Type", "Authorization"],
-  credentials: true, // 👈 required for cookies/JWT refresh
+  credentials: true,
 };
 
 app.use(
