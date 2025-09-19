37fafcd43d67e47a650d1f2e99e76b5d40407517
Your Name <jackdanielberry@gmail.com>
2025-09-19 11:46:49 +0100

    added CRUD routes for events

--- src/routes/eventRoutes.js
+++ src/routes/eventRoutes.js
@@ -5,19 +5,24 @@ const authenticateToken = require("../middleware/authMiddleware");
 
 // Public
 router.get("/", eventController.getEvents);
+router.get("/:id", eventController.getEventById);
 
 // Protected
 router.post("/", authenticateToken, eventController.createEvent);
+router.put("/:id", authenticateToken, eventController.updateEvent);
+router.delete("/:id", authenticateToken, eventController.deleteEvent);
+
 router.post(
   "/:id/register",
   authenticateToken,
   eventController.registerInterest
 );
-router.get("/user/:id", authenticateToken, eventController.getUserEvents);
 router.delete(
   "/:id/unregister",
   authenticateToken,
   eventController.unregisterInterest
 );
+
+router.get("/user/:id", authenticateToken, eventController.getUserEvents);
 
 module.exports = router;
