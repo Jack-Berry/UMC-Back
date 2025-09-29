8ece30e40ea5f9cc2a6c7fc5a8e48b8a3eab150c
Your Name <jackdanielberry@gmail.com>
2025-09-29 12:11:57 +0100

    added CRUD routes for tags

--- src/routes/tagsRoutes.js
+++ src/routes/tagsRoutes.js
@@ -1,10 +1,22 @@
 // src/routes/tags.js
 const express = require("express");
-const { getTags } = require("../controllers/tagsController");
+const {
+  getTags,
+  createTag,
+  updateTag,
+  deleteTag,
+} = require("../controllers/tagsController");
+const authenticateToken = require("../middleware/authMiddleware");
+const requireAdmin = require("../middleware/requireAdmin");
 
 const router = express.Router();
 
 // Public: GET /api/tags
 router.get("/", getTags);
+
+// Admin-only: CRUD
+router.post("/", authenticateToken, requireAdmin, createTag);
+router.put("/:id", authenticateToken, requireAdmin, updateTag);
+router.delete("/:id", authenticateToken, requireAdmin, deleteTag);
 
 module.exports = router;
